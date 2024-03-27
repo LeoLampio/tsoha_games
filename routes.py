d56595b8cf197a056b5a1e85c678ef4cd9eda971
Leo Lampio <149969862+LeoLampio@users.noreply.github.com>
2024-03-27 13:51:53 +0200

    Finished login system (with db)

--- routes.py
+++ routes.py
@@ -16,7 +16,8 @@ def login():
     if (request.method == "POST"):
         username = request.form["username"]
         password = request.form["password"]
-        users.login(username, password)
+        if (not users.login(username, password)):
+            return render_template("login.html", message="Incorrect username or password.")
         return redirect("/")
 
 @app.route("/logout")
@@ -33,7 +34,19 @@ def register():
         username = request.form["username"]
         password1 = request.form["password1"]
         password2 = request.form["password2"]
+
+        if (len(username) < 1 or len(username) > 20):
+            return render_template("register.html", message="Username should have 1-20 characters.")
         if (password1 != password2):
             return render_template("register.html", message="Passwords don't equal.")
-        users.login(username, password1)
+        if (len(password1) < 5):
+            return render_template("register.html", message="Password needs to be atleast 5 characters long.")
+        
+        value = users.register(username, password1)
+        
+        if (value == 1):
+            return render_template("register.html", message="Username is taken.")
+        elif (value == 2):
+            return render_template("register.html", message="Login error.")
+
         return redirect("/")
--- users.py
+++ users.py
@@ -1,12 +1,51 @@
 from db import db
 from sqlalchemy.sql import text
-from flask import session
+from flask import session, request, abort
+from werkzeug.security import check_password_hash, generate_password_hash
+import secrets
 
 def login(username, password):
+    sql = "SELECT password, id FROM users WHERE username =:username"
+    result = db.session.execute(text(sql), {"username": username}).fetchone()
+
+    if (result == None):
+        # Invalid username
+        return False
+    
+    if (not check_password_hash(result[0], password)):
+        # Invalid password
+        return False
+    
+    session["user_id"] = result[1]
     session["username"] = username
+    session["csrf_token"] = secrets.token_hex(16)
+    return True
 
 def logout():
+    del session["user_id"]
     del session["username"]
+    del session["csrf_token"]
 
 def register(username, password):
-    pass
+    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+    password_hash = generate_password_hash(password)
+    try:
+        db.session.execute(text(sql), {"username": username, "password": password_hash})
+        db.session.commit()
+    except:
+        # Username is taken
+        return 1
+    
+    if (login(username, password)):
+        # No error
+        return 0
+    else:
+        # Login error
+        return 2
+    
+def get_user_id():
+    return session.get("user_id", 0)
+
+def check_csrf():
+    if (session["csrf_token"] != request.form("csrf_token")):
+        abort(403)
